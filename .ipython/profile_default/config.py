a63f71f6568426e8c29cc88f0fb206e3ad08d83e
evan <evan@evanw.org>
2018-03-25 00:35:00 -0500

    pass update, bookmarks ipython

--- .ipython/profile_default/config.py
+++
@@ -1,5 +0,0 @@
-c.InteractiveShellApp.exec_lines = []
-c.InteractiveShellApp.exec_lines.append('%load_ext autoreload')
-c.InteractiveShellApp.exec_lines.append('%autoreload 2')
-%load_ext autoreload
-%autoreload 2
---
+++ .ipython/profile_default/ipython_config.py
@@ -0,0 +1,19 @@
+c = get_config()
+
+c.TerminalIPythonApp.display_banner = True
+c.InteractiveShellApp.log_level = 20
+c.InteractiveShellApp.exec_lines = [
+    'import numpy as np',
+    'from matplotlib import pyplot as plt',
+    '%load_ext autoreload',
+    '%autoreload 2',
+    'np.set_printoptions(linewidth=9999, precision=2, suppress=True)',
+    '%matplotlib',
+]
+c.InteractiveShell.autoindent = True
+c.InteractiveShell.confirm_exit = False
+c.InteractiveShell.deep_reload = True
+import os
+editor =  os.environ.get('EDITOR')
+if editor:
+    c.InteractiveShell.editor = editor
