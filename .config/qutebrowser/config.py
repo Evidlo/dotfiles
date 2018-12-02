0442327725a1c759bd11c5b2c999f54a10ffa0cd
evan <evan@evanw.org>
2018-12-01 21:07:09 -0600

    qutebrowser, password, readline, paste sync

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -5,5 +5,6 @@ c.completion.use_best_match = True
 
 config.bind('<Ctrl-H>', 'rl-unix-filename-rubout', mode='command')
 config.bind(';m', 'hint links spawn -d mpv {hint-url}')
+config.bind(';o', 'hint links spawn -d xdg-open {hint-url}')
 config.bind(';j', 'config-cycle content.javascript.enabled;;reload -f')
 
