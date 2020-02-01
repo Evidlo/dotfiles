8551ecc332342b35ebf3ebca1d979deea0d2c859
evan <evan@evanw.org>
2020-02-01 02:57:34 -0600

    add tqdm reset

--- .ipython/profile_default/startup/tqdm.py
+++ .ipython/profile_default/startup/tqdm.py
@@ -1,7 +1,7 @@
 from IPython import get_ipython
-import tqdm
 
 def tqdm_clear(*args, **kwargs):
-    tqdm._instances.clear()
+    from tqdm import tqdm
+    getattr(tqdm, '_instances', {}).clear()
 
 get_ipython().events.register('post_execute', tqdm_clear)
