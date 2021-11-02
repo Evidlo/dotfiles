41a840d9ac4daeaa0d67a15918dbda0c292c591a
evan <evan@evanw.org>
2021-11-02 14:58:28 -0500

    qutebrowser shortcuts

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,9 +1,15 @@
-c.url.searchengines = {'DEFAULT': 'https://google.com/search?q={}', '!wiki': 'http://wikipedia.org/wiki/Special:Search/{}'}
+config.load_autoconfig()
+
+c.url.searchengines = {
+    'DEFAULT': 'https://google.com/search?q={}',
+    '!wiki': 'http://wikipedia.org/wiki/Special:Search/{}',
+    '!etym': 'https://www.etymonline.com/search?q={}'
+}
 c.url.default_page = 'https://google.com/'
 c.url.start_pages = ['https://google.com']
 c.completion.use_best_match = True
 c.completion.web_history.max_items = 10000
-c.content.host_blocking.whitelist = ['www.rapidvideo.com']
+c.content.blocking.whitelist = ['www.rapidvideo.com']
 c.content.geolocation = False
 c.content.notifications = False
 c.input.insert_mode.leave_on_load = False
