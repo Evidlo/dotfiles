a105e5194e59c77341ddf7619386fe5dd17809c9
evan <evan@evanw.org>
2021-11-02 14:59:51 -0500

    numpy precision and other things

--- .ipython/profile_default/ipython_config.py
+++ .ipython/profile_default/ipython_config.py
@@ -1,42 +1,42 @@
 from IPython.terminal.prompts import Prompts, Token
-import os
-import sys
 
 c = get_config()
 
+# show python version on start
 c.TerminalIPythonApp.display_banner = True
 c.InteractiveShellApp.log_level = 20
 c.InteractiveShellApp.exec_lines = [
+    # import numpy and limit displayed precision
     'import numpy as np',
+    'np.set_printoptions(precision=3, suppress=True)',
     'import ipdb',
     'from matplotlib import pyplot as plt',
     '%load_ext autoreload',
-    '%load_ext ipython_cells',
     '%autoreload 2',
-    'np.set_printoptions(precision=3, suppress=True)',
-    '%matplotlib',
+    '%load_ext ipython_cells',
 ]
+# '%matplotlib',
 
-# custom prompt
+# cleaner prompt, hide "In[x]", "Out[x]"
 class MyPrompt(Prompts):
      def in_prompt_tokens(self, cli=None):
          return [(Token.Prompt, '>>> ')]
+     def out_prompt_tokens(self):
+         return [(Token.Prompt, '')]
+c.TerminalInteractiveShell.prompts_class = MyPrompt
+
+c.InteractiveShellApp.exec_lines.append('np.seterr(all=\'print\')')
+c.InteractiveShell.autoindent = True
+c.InteractiveShell.confirm_exit = False
+# set vi editin mode
+c.TerminalInteractiveShell.editing_mode = 'vi'
+c.TerminalInteractiveShell.prompt_includes_vi_mode = False
+# increase history length
+c.TerminalInteractiveShell.history_length = 20000
+c.TerminalInteractiveShell.history_load_length = 20000
 
 
-
-
-if sys.version_info.major == 3:
-    import sys
-    c.InteractiveShellApp.exec_lines.append('np.seterr(all=\'print\')')
-    c.InteractiveShell.autoindent = True
-    c.InteractiveShell.confirm_exit = False
-    c.InteractiveShell.deep_reload = True
-    c.TerminalInteractiveShell.editing_mode = 'vi'
-    c.TerminalInteractiveShell.prompt_includes_vi_mode = False
-    c.TerminalInteractiveShell.history_length = 20000
-    c.TerminalInteractiveShell.history_load_length = 20000
-    c.TerminalInteractiveShell.prompts_class = MyPrompt
-    import os
-    editor =  os.environ.get('EDITOR')
-    if editor:
-        c.InteractiveShell.editor = editor
+# import os
+# editor =  os.environ.get('EDITOR')
+# if editor:
+#     c.InteractiveShell.editor = editor
